cbd405709f4e953f202cb38bc796313f75fcf4d3
Miriam_Sallnäs <miriamfly54@hotmail.com>
2018-09-05 09:36:30 +0200

    pull from Rupert and solve merge conflict

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
-document.addEventListener('DOMContentLoaded',() =>{
-    const addContentForm = document.querySelector('.new-contact-form')
-
-    addContentForm.addEventListener('submit', event => {
-        event.preventDefault()
-        const storage = window.localStorage
-        const {
-            name,
-            email,
-            phone,
-            company,
-            notes,
-            twitter,
-        } = addContentForm.elements
-
-        const contact = {
-            id: Date.now(),
-            name: name.value,
-            email: email.value,
-            phone: phone.value,
-            company: company.value,
-            notes: notes.value,
-            twitter: twitter.value,
-        }
-        
-        console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
-        storage.setItem('contacts', JSON.stringify([contact]))
+document.addEventListener('DOMContentLoaded', () => {
+    const addContactForm = document.querySelector('.new-contact-form')
+  
+    addContactForm.addEventListener('submit', event => {
+      event.preventDefault()
+      const storage = window.localStorage
+  
+      const {
+        name,
+        email,
+        phone,
+        company,
+        notes,
+        twitter,
+      } = addContactForm.elements
+  
+      const contact = {
+        id: Date.now(),
+        name: name.value,
+        email: email.value,
+        phone: phone.value,
+        company: company.value,
+        notes: notes.value,
+        twitter: twitter.value,
+      }
+  
+      console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
+      storage.setItem('contacts', JSON.stringify([contact]))
     })
-})
+  })
