adc6b82b3749e72b2a2150c45d376a7a71dfb225
rupertlion <rupertlion@gmail.com>
2018-09-05 11:40:54 +0200

    Added missing button changes

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -13,7 +13,7 @@ Then('I should see {string}', async function(content) {
 })
 
 When('I click {string}', async function(string) {
-  return await this.clickOnAddContactBtn()
+  return await this.clickOnButton(string)
 })
 
 Then('I fill in {string} with {string}', async function(field, content) {
