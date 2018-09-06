b12d7b055aecedddadf7446c927cc04c25f75160
rupertlion <rupertlion@gmail.com>
2018-09-06 17:31:28 +0200

    Contact cards intial styling

--- src/app.js
+++ src/app.js
@@ -7,22 +7,27 @@ const renderContacts = () => {
     div.innerHTML = ''
     const ul = document.createElement('ul')
     contacts.forEach(contact => {
-      let li = document.createElement('li')
-      li.innerHTML = `
-      <div class="card">
-        <div class="image">
-          <img src="https://ca-address-book.herokuapp.com/images/pine.jpg" />
-        </div>
-        <div class="content">
-          <h1>${ contact.name }</h1>
-          <h2>${ contact.company}</h2>
-          <p>${ contact.notes }</p>
-          ${ contact.email } |
-          <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
-        </div>
-      </div>
+      let contactcard = document.createElement('contactcard')
+        contactcard.innerHTML = `
+            <div class="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden mt-6 py-6">
+                <div class="sm:flex sm:items-center px-6 py-4">
+                    <img class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="https://avatars2.githubusercontent.com/u/4323180?s=400&u=4962a4441fae9fba5f0f86456c6c506a21ffca4f&v=4" alt="">
+                    <div class="text-center sm:text-left sm:flex-grow">
+                        <div class="mb-4">
+                            <p class="text-xl leading-tight">${ contact.name }</p>
+                            <p class="text-m leading-tight text-grey-dark">${ contact.company}</p>
+                            <p class="text-s leading-tight text-grey-dark">${ contact.notes}</p>
+                            <p class="text-sm leading-tight text-grey-dark">${ contact.email}</p>
+                            <a class="text-sm leading-tight text-grey-dark" href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Message</button>
+                    </div>
+                </div>
+            </div>
     `
-    ul.appendChild(li)
+    ul.appendChild(contactcard)
     })
     div.appendChild(ul)
   } else {
